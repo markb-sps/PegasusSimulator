fixed rotation bug

extensions/pegasus.simulator/pegasus/simulator/logic/vehicles/vehicle.py
@@ -39,8 +39,9 @@ def get_world_transform_xform(prim: Usd.Prim):
         - Scale vector.
     """
     world_transform: Gf.Matrix4d = omni.usd.get_world_transform_matrix(prim)
-    rotation: Gf.Rotation = world_transform.ExtractRotation()
-    return rotation
+
+    # rotation: Gf.Rotation = world_transform.ExtractRotation()
+    return world_transform.ExtractRotationQuat()
 
 
 class Vehicle(Robot):
@@ -207,7 +208,14 @@ class Vehicle(Robot):
 
         # Get the attitude according to the convention [w, x, y, z]
         prim = self._world.stage.GetPrimAtPath(self._stage_prefix + "/body")
-        rotation_quat = get_world_transform_xform(prim).GetQuaternion()
+        rotation_quat = get_world_transform_xform(prim)
+        # rotation_quat = get_world_transform_xform(prim).GetQuaternion()
+
+        testq = get_world_transform_xform(prim)
+        # rot = Rotation.from_quat(np.array([testq[]]))
+        # euler_angles = rot.as_euler('xyz', degrees=True)
+        
+
         rotation_quat_real = rotation_quat.GetReal()
         rotation_quat_img = rotation_quat.GetImaginary()
 